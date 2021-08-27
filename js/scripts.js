a527ff5f259aa874291c5b9eabdebdbf43673c8f
Cameron Kronsbein <cameron.kronsbein@gmail.com>
2021-08-27 09:31:14 -0700

    WIP business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
+//Business Logic
 
+let toppings = [];
+
+class Pizza {
+  pizzaStyle (crust, sauce, toppings, cheese) {
+    this.crust = crust;
+    this.sauce = sauce;
+    this.toppings = toppings;
+    this.cheese = cheese;
+  }
+}
