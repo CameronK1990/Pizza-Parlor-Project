d48650716cc132bf1df462376e2e7801b01343fc
Cameron Kronsbein <cameron.kronsbein@gmail.com>
2021-08-27 11:55:07 -0700

    ADD business logic and html return

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,35 @@
 //Business Logic
 
-let toppings = [];
-
-class Pizza {
-  pizzaStyle (crust, sauce, toppings,) {
+class pie {
+  pieStyle (crust, sauce, toppings,) {
     this.crust = crust;
     this.sauce = sauce;
     this.toppings = toppings;
   }
+}
+
+Pie.prototype.crust = function() {
+  if (this.crust === "Personal") {
+    return 6;
+  } else if (this.crust === "Medium") {
+    return 8;
+  } else if (this.crust === "Large") {
+    return 12;
+  } else {
+    alert ("Select a size");
+  }
+}
+
+Pie.prototype.toppings = function () {
+if (this.toppings.length !=0) {
+   let toppingsTotal = this.toppings.length *1;return toppingsTotal;
+  } else {
+    alert ("Choose Toppings");
+  }
+}
+
+Pie.prototype.comboCost = function (crustTotal, toppingsTotal) {
+  let totalCost= crustTotal + toppingsTotal;
+  $(this.#displayTotalCost).html(totalCost);
 }
 
